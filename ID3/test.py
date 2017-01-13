98ab5928513c1a1e9955aef1a7d9dd84d9185fef
Yang Feng <szufengyang@gmail.com>
2017-01-13 12:46:29 +0800

    Update test.py

--- ID3/test.py
+++ ID3/test.py
@@ -1,1 +1,14 @@
+from ID3 import id3
 
+if __name__ == '__main__':
+	X = [[0, 1, 1, 0, 1],
+		[1, 2, 0, 1, 0],
+		[1, 0, 0, 0, 1],
+		[1, 1, 0, 1, 1],
+		[2, 1, 1, 0, 1]]
+	y = ['yes','yes','no','no','no']
+  
+	clf = id3()
+	clf.fit(X,y)
+  
+	print clf.predict(X)
